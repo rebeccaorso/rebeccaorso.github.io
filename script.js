1a55ab259a196d5cf5503617272999c53172a6b1
Rebecca Orso Giacone <135012814+rebeccaorso@users.noreply.github.com>
2024-03-05 11:43:01 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let aTeatro = document.querySelector("#aTeatro");
 let name = document.querySelector("#name");
 let email = document.querySelector("#email");
-let message = document.querySelector("#message");
+
 
 document.addEventListener("DOMContentLoaded", function () {
     let card = document.getElementById("aTeatro");
@@ -13,6 +13,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
 
 function sendEmail() {
+    let message = document.querySelector("#message").value;
     let mailtoLink = "mailto:rebeccaorso@gmail.com?subject=Richiesta di contatto dal portfolio GitHub" + encodeURIComponent(message);
 
     window.location.href = mailtoLink;
