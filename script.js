ad5fa9ffc161aa34eb61664efadef7f707e645a0
Rebecca Orso Giacone <135012814+rebeccaorso@users.noreply.github.com>
2024-03-05 16:06:07 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -11,10 +11,11 @@ document.addEventListener("DOMContentLoaded", function () {
     });
 });
 
-
 function sendEmail() {
     let message = document.getElementById("message").value;
-    let mailtoLink = "mailto:rebeccaorso@gmail.com?subject=Richiesta di contatto dal portfolio GitHub&body=" + encodeURIComponent(message);
+    let mailtoLink = "mailto:rebeccaorsogiacone@gmail.com?subject=Richiesta di contatto dal portfolio GitHub&body=" + encodeURIComponent(message);
 
     window.location.href = mailtoLink;
 }
+
+
