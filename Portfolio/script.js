6b4efdd8a165b734dfe204222735bbe705fe8f98
Rebecca Orso Giacone <135012814+rebeccaorso@users.noreply.github.com>
2024-03-05 09:58:06 +0100

    Delete Portfolio directory

--- Portfolio/script.js
+++
@@ -1,9 +0,0 @@
-let aTeatro = document.querySelector("#aTeatro");
-
-document.addEventListener("DOMContentLoaded", function () {
-    let card = document.getElementById("aTeatro");
-    card.addEventListener("click", function () {
-        let url = "https://github.com/rebeccaorso/Progetto_Finale_Teatro";
-        window.open(url, "_blank");
-    });
-});
